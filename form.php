552e37d43bebae778e6785b22fdae90da9358a26
Rodney Martinez Molina <2617230+RodneyMartinez@users.noreply.github.com>
2022-10-21 12:14:32 -0600

    Adding a form.php file

---
+++ form.php
@@ -0,0 +1,18 @@
+<!-- <!DOCTYPE html> -->
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PHP Form</title>
+</head>
+<body>
+	<form action="form_process.php" method="post">
+		<input type="text" placeholder="Enter your username" name="username">
+		<br>
+		<input type="password" placeholder="Enter your password" name="password">
+		<br>
+		<input type="submit" value="Submit" name="submit">
+	</form>
+
+</body>
+</html>
